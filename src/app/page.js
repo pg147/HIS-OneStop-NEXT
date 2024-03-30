332556adb95fb5d538355de07893a280427f6b21
pg147 <xyzprathamesh147@gmail.com>
2024-03-31 01:11:16 +0530

    Conflicts resolved

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,4 @@
-import ControlBar from "@/components/controlsField/controlsBar";
-import { DomainsBar } from "@/components/domainsField/domainsField";
-import SearchBar from "@/components/searchField/searchField";
-import Signup from "@/app/signup/page";
+import Login from "./login/page";
 
 export default function Home() {
   return (
