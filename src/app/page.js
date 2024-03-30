d3d8db5416fac778272add37513ded75ec848445
nupurhada <hadanupur@gmail.com>
2024-03-31 01:02:52 +0530

    create post overlay page added

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,13 @@ import ControlBar from "@/components/controlsField/controlsBar";
 import { DomainsBar } from "@/components/domainsField/domainsField";
 import SearchBar from "@/components/searchField/searchField";
 import Signup from "@/app/signup/page";
+import CreatePost from "@/components/createPost/page";
 
 export default function Home() {
   return (
     <div className="flex justify-between p-8">
-      <Signup />
+      {/* <Signup /> */}
+      <CreatePost />
       {/* <DomainsBar />
       <SearchBar />
       <ControlBar /> */}
