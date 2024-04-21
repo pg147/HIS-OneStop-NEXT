83e11059158b184bc930745ff74ebfc665ce47b6
pg147 <xyzprathamesh147@gmail.com>
2024-04-21 17:07:09 +0530

    Interface Changes

--- src/app/page.js
+++ src/app/page.js
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <>
       {/* <Login /> */}
-      {/* <Interface /> */}
-      <Signup />
+      <Interface />
+      {/* <Signup /> */}
     </>
   );
 }
